80586963bf048396329eb51672c776f6acd6e25d
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-04-03 15:27:19 -0300

    Refatoração da service do leaderboard

--- app/backend/src/database/service/leaderboardService.ts
+++ app/backend/src/database/service/leaderboardService.ts
@@ -3,8 +3,8 @@ import { IFullLeaderbordResult, ILeaderbordSqlResult } from '../../interfaces/le
 import { awayLeaderBoardQuery, homeLeaderBoardQuery } from '../../sequelizeMySqlQuerys/leaderboard';
 
 export default class LeaderboardService {
-  public getHomeTeams = async (): Promise<IFullLeaderbordResult[]> => {
-    const [result] = await sequelize.query(homeLeaderBoardQuery) as ILeaderbordSqlResult[][];
+  private getResultLeaderboard = async (query:string):Promise<IFullLeaderbordResult[]> => {
+    const [result] = await sequelize.query(query) as ILeaderbordSqlResult[][];
     const fullLeaderbord = result.map((team) => {
       const efficiencyPercentage = ((team.totalPoints / (team.totalGames * 3))).toFixed(2);
       return { ...team, efficiency: efficiencyPercentage };
@@ -12,14 +12,9 @@ export default class LeaderboardService {
     return fullLeaderbord;
   };
 
-  public getAwayTeams = async (): Promise<IFullLeaderbordResult[]> => {
-    const [result] = await sequelize.query(awayLeaderBoardQuery) as ILeaderbordSqlResult[][];
-    const fullLeaderbord = result.map((team) => {
-      const efficiencyPercentage = ((team.totalPoints / (team.totalGames * 3))).toFixed(2);
-      return { ...team, efficiency: efficiencyPercentage };
-    });
-    console.log('asas');
+  public getHomeTeams = async (): Promise<IFullLeaderbordResult[]> => this
+    .getResultLeaderboard(homeLeaderBoardQuery);
 
-    return fullLeaderbord;
-  };
+  public getAwayTeams = async (): Promise<IFullLeaderbordResult[]> => this
+    .getResultLeaderboard(awayLeaderBoardQuery);
 }
